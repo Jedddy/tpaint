e648ec907398aff0bcebb3f20fd4ddf5fd009043
Jedddy <jedbalita25@gmail.com>
2023-12-29 11:36:57 +0800

    feat: add eraser

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ color_positions = {}
 class Paint:
     window: "curses._CursesWindow"
 
-    def __call__(self, window: "curses._CursesWindow"):
-        curses.curs_set(0)
-        curses.mousemask(curses.ALL_MOUSE_EVENTS)
+    def load_colors(self):
         curses.start_color()
         curses.use_default_colors()
-
-        # window.nodelay(True)
-        maxy, maxx = window.getmaxyx()
 
         try:
             for c in range(curses.COLORS):
@@ -22,33 +17,49 @@ class Paint:
         except (ValueError, curses.error):
             pass
 
+    def show_palette(self):
+        for color, posx in zip(
+            range(self.color_range),
+            range((self.maxx // 2) - (self.color_range  * 2), self.maxx, 4)
+        ):
+            pair = curses.color_pair(color + 1)
+            color_positions[posx] = pair
+            color_positions[posx + 1] = pair  # 1 color takes 2 positions
+            self.window.addstr(self.maxy - 1, posx, "██", pair)
+
+        for idx, letter in enumerate("ERASER"):
+            self.window.addstr(self.maxy - 1, idx, letter)
+            color_positions[idx] = -2 # i picked -2 for the color for eraser
+
+    def __call__(self, window: "curses._CursesWindow"):
+        curses.curs_set(0)
+        curses.mousemask(curses.ALL_MOUSE_EVENTS)
+
+        self.load_colors()
+        window.nodelay(True)
+        self.maxy, self.maxx = window.getmaxyx()
+
         self.window = window
-        color_range = 256 // 32
+        self.color_range = 256 // 32
         pressed = False
         current_color = - 1
 
-        while True:
-            for color, posx in zip(
-                range(color_range),
-                range((maxx // 2) - (color_range  * 2), maxx, 4)
-            ):
-                pair = curses.color_pair(color + 1)
-                color_positions[posx] = pair
-                color_positions[posx + 1] = pair  # 1 color takes 2 positions
-                self.window.addstr(maxy - 1, posx, "██", pair)
+        self.show_palette()
 
+        while True:
             key = self.window.getch()
 
             if key == curses.KEY_RESIZE:
-                maxy, maxx = self.window.getmaxyx()
+                self.maxy, self.maxx = self.window.getmaxyx()
                 self.window.clear()
+                self.show_palette()
 
             if key == curses.KEY_MOUSE:
                 _, x, y, _, bstate = curses.getmouse()
 
                 if (
                     bstate & curses.BUTTON1_CLICKED and
-                    y == maxy - 1 and
+                    y == self.maxy - 1 and
                     x in color_positions
                 ):
                     current_color = color_positions[x]
@@ -59,11 +70,19 @@ class Paint:
                 if bstate & curses.BUTTON1_RELEASED:
                     pressed = False
 
-                if pressed and y < maxy - 1:
-                    self.window.addstr(y, x, "██", current_color)
+                if pressed and y < self.maxy - 1:
+                    if current_color == -2:
+                        self.window.addstr(y, x, "  ")
+                    else:
+                        self.window.addstr(y, x, "██", current_color)
 
-            elif key == ord("q"):
+            if key == ord("q"):
+                self.window.clear()
                 break
+
+            if key == ord("c"):
+                self.window.clear()
+                self.show_palette()
 
             self.window.refresh()
 
